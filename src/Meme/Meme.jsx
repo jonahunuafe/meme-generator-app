b26ec222799655e80a30ee2596b3c2c05683f28a
Jonah Unuafe <fejisfejiro@gmail.com>
2024-04-01 13:05:37 +0100

    useState hook in action

--- src/Meme/Meme.jsx
+++ src/Meme/Meme.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Meme = () => {
+    const [memes, setMemes] = useState();
+
     const fetchMeme = async () => {
         const res = await fetch("https://api.imgflip.com/get_memes");
         const data = await res.json();
