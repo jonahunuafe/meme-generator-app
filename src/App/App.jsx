98da8dc091d98d598baafc4678cae343956160a3
Jonah Unuafe <fejisfejiro@gmail.com>
2024-04-01 12:58:47 +0100

    meme generator app

---
+++ src/App/App.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+// import styles from "./styles.module.css";
+
+import { Meme } from "../Meme/Meme"
+
+export const App = () => {
+
+  return (
+    <>
+      <Meme />
+    </>
+  )
+}
