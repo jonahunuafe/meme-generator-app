83d2dd403779c5be1100c8ce4223e91fab1aa062
Jonah Unuafe <fejisfejiro@gmail.com>
2024-04-01 13:01:33 +0100

    fetching meme

--- src/Meme/Meme.jsx
+++ src/Meme/Meme.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 export const Meme = () => {
+    const fetchMeme = async () => {
+        const res = await fetch("https://api.imgflip.com/get_memes");
+        const data = await res.json();
+        console.log(data);
+    }
 
     return (
         <div>Meme</div>
