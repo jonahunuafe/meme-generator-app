18900158436cd2be29091eab2ea514a438baebf1
Jonah Unuafe <fejisfejiro@gmail.com>
2024-04-01 13:37:02 +0100

    added some styling to Meme component

--- src/Meme/Meme.jsx
+++ src/Meme/Meme.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import styles from "./meme.module.css"
 
 export const Meme = () => {
-    const [memes, setMemes] = useState();
+    const [memes, setMemes] = useState([]);
 
     const fetchMeme = async () => {
         const res = await fetch("https://api.imgflip.com/get_memes");
-        const data = await res.json();
-        console.log(data);
+        const memesData = await res.json();
+        setMemes(memesData.data.memes);
     }
 
     useEffect(() => {
@@ -14,6 +15,11 @@ export const Meme = () => {
     }, []);
 
     return (
-        <div>Meme</div>
-    )
+      memes.length ? 
+      <div className={styles.container}>
+        <button className={styles.skip}>Skip</button>
+        <img src={memes[0].url} />
+      </div> : 
+      <></>  
+    );
 }
