e7606ce819933778d1d2bf78df882c7e8e72b1b1
Jonah Unuafe <fejisfejiro@gmail.com>
2024-04-02 16:10:50 +0100

    remove MemeGenerated component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,16 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Meme } from "../Meme/Meme"
-import { MemeGenerated } from '../MemeGenerated/MemeGenerated';
 
 export const App = () => {
   const router = createBrowserRouter([
-    { path: "/", element: <Meme /> },
-    {path: "/generated", element: <MemeGenerated />}
+    { path: "/", element: <Meme /> }
   ]);
 
   return (
     <div>
-      <h1>Meme Creator</h1>
+      <h1>Meme Generator</h1>
       <RouterProvider router={router} />
     </div>
   );
--- src/Meme/Meme.jsx
+++ src/Meme/Meme.jsx
@@ -1,46 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react'
 
 import styles from "./meme.module.css"
 
 export const Meme = () => {
     const [memes, setMemes] = useState([]);
     const [memeIndex, setMemeIndex] = useState(0);
-    const [captions, setCaptions] = useState([]);
-
-   const history = useNavigate();
-
-    const updateCaption = (e, index) => {
-        const text = e.target.value || "";
-        setCaptions(
-            captions.map((c, i) => {
-                if(index === i) {
-                    return text;
-                } else {
-                    return c;
-                }
-            })
-        );
-    }
-
-    const generateMeme = () => {
-        const currentMeme = memes[memeIndex];
-        const formData = new FormData();
-
-        formData.append("username", "");
-        formData.append("password", "");
-        formData.append("template_id", currentMeme.id);
-        captions.forEach((caption, index) => formData(`boxes[${index}][text]`, caption));
-
-        fetch('https://api.imgflip.com/caption_image', {
-            method: 'POST',
-            body: formData
-            }).then(res => {
-            res.json().then(res => {
-            history.push(`/generated?url=${res.data.url}`);
-            });
-        });
-    }
 
     const shuffleMemes = (array) => {
         for (let i = array.length - 1; i > 0; i--) {
@@ -62,23 +26,10 @@ export const Meme = () => {
         fetchMeme();
     }, []);
 
-    useEffect(() => {
-        if (memes.length) {
-            // An array of empty strings
-            setCaptions(Array(memes[memeIndex].box_count).fill(""));
-        }
-    }, [memeIndex, memes]);
-
     return (
       memes.length ? 
       <div className={styles.container}>
-        <button onClick={generateMeme} className={styles.generate}>Generate</button>
         <button onClick={() => setMemeIndex(memeIndex + 1)} className={styles.skip}>Skip</button>
-        {
-            captions.map((caption, index) => (
-                <input onChange={(e) => updateCaption(e, index)} key={index} />
-            ))
-        }
         <img src={memes[memeIndex].url} />
       </div> : 
       <></>  
