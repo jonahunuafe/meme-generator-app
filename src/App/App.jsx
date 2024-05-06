68e55b0275a2855782d89f30daef9fb2bf6f53d2
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-06 12:54:10 +0100

    refactor Meme component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,12 +3,11 @@ import { Meme } from "../Meme/Meme"
 
 export const App = () => {
   const router = createBrowserRouter([
-    { path: "/", element: <Meme /> }
+    { path: "/", element: <Meme /> },
   ]);
 
   return (
     <div>
-      <h1>Meme Generator</h1>
       <RouterProvider router={router} />
     </div>
   );
--- src/Meme/Meme.jsx
+++ src/Meme/Meme.jsx
@@ -2,36 +2,37 @@ import { useEffect, useState } from 'react'
 
 import styles from "./meme.module.css"
 
+
 export const Meme = () => {
-    const [memes, setMemes] = useState([]);
-    const [memeIndex, setMemeIndex] = useState(0);
+  const [memes, setMemes] = useState([]);
+  const [memeIndex, setMemeIndex] = useState(0);
 
-    const shuffleMemes = (array) => {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * i);
-          const temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-        }
-    };
+  //function to shuffle memes
+  const shuffleMemes = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
 
-    useEffect(() => {
-        const fetchMeme = async () => {
-            const res = await fetch("https://api.imgflip.com/get_memes");
-            const memesData = await res.json();
-            shuffleMemes(memesData.data.memes);
-            setMemes(memesData.data.memes);
-        }
+  useEffect(() => {
+    const fetchMeme = async () => {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const memesData = await res.json();
+      shuffleMemes(memesData.data.memes);
+      setMemes(memesData.data.memes);
+    }
 
-        fetchMeme();
-    }, []);
+    fetchMeme();
+  }, []);
 
-    return (
+  return (
       memes.length ? 
       <div className={styles.container}>
         <button onClick={() => setMemeIndex(memeIndex + 1)} className={styles.skip}>Skip</button>
         <img src={memes[memeIndex].url} />
-      </div> : 
-      <></>  
-    );
+      </div> : <></>
+  );
 }
