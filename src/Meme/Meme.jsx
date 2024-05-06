087e8d41f4dee429af615b30a43e3c8847a7e091
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-06 13:10:40 +0100

    implememt function to generate meme and navigate to meme generated using query params

--- src/Meme/Meme.jsx
+++ src/Meme/Meme.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import styles from "./meme.module.css"
 
@@ -7,6 +8,12 @@ export const Meme = () => {
   const [memes, setMemes] = useState([]);
   const [memeIndex, setMemeIndex] = useState(0);
   const [captions, setCaptions] = useState([]);
+
+  const navigate = useNavigate();
+
+  function navigateHandler(urlLink) {
+    navigate(urlLink)
+  }
 
   const updateCaption = (e, index) => {  //index of input, i index of what we are mapping
     const text = e.target.value || "";
@@ -20,6 +27,26 @@ export const Meme = () => {
       })
     )
   };
+
+  const generateMeme = () => {
+    const currentMeme = memes[memeIndex];
+    const formData = new FormData();
+
+    formData.append("username", "JonahUnuafe")
+    formData.append("password", "perfectpurity")
+    formData.append("template_id", currentMeme.id)
+    captions.forEach((c, index) => formData.append(`boxes[${index}][text]`, c))
+
+    fetch("https://api.imgflip.com/caption_image", {
+      method: "POST",
+      body: formData
+    }).then(res => {
+      res.json().then(res => {
+        navigateHandler(`/generated?url=${res.data.url}`)
+      })
+    })
+  }
+
 
   //function to shuffle memes
   const shuffleMemes = (array) => {
@@ -52,6 +79,7 @@ export const Meme = () => {
   return (
       memes.length ? 
       <div className={styles.container}>
+        <button onClick={generateMeme} className={styles.skip}>Generrate</button>
         <button onClick={() => setMemeIndex(memeIndex + 1)} className={styles.skip}>Skip</button>
         {
           captions.map((c, index) => (
