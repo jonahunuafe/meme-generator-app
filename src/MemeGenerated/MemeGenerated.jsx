2054f43f7c633a2d4c82fe91e599d657d4156b9a
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-06 13:34:08 +0100

    added button to navigate back to the memes component

--- src/MemeGenerated/MemeGenerated.jsx
+++ src/MemeGenerated/MemeGenerated.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const MemeGenerated = () => {
+   const navigate = useNavigate();
    const location = useLocation();
    const url = new URLSearchParams(location.search).get("url");
 
   return (
       <div style={{textAlign: "center"}}>
+         <button onClick={() => navigate("/")}>Make more memes</button>
          { url && <img src={url} /> }
       </div>
   )
